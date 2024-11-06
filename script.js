453c0e8dc5e48554818297eb93b2dc38369e7347
svidlak <svid320@gmail.com>
2024-11-06 12:04:40 +0200

    test: Ios solution for notifications

--- script.js
+++ script.js
@@ -222,8 +222,7 @@ function insertHasntSeenBeforeStock(stockName, changePercentage) {
  * @param {string} changePercentage - The change percentage of the stock.
  */
 function notifyAboutNewStock(stockName, changePercentage) {
-    if(!Notification) return;
-    if (Notification.permission === "granted") {
+    if (Notification && Notification.permission === "granted") {
         new Notification(`New Stock Added: ${stockName}`, {
             body: `${stockName} has been added with a ${changePercentage} change.`
         });
@@ -234,9 +233,7 @@ function notifyAboutNewStock(stockName, changePercentage) {
  * Requests permission for desktop notifications if not already granted.
  */
 function requestNotificationPermission() {
-    if(!Notification) return;
-
-    if (Notification.permission === "default") {
+    if (Notification && Notification.permission === "default") {
         Notification.requestPermission();
     }
 }
