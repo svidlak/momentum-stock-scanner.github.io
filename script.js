11c92e13b8e98708a126dc729634727c70d23d75
Max Svidlo <svid320@gmail.com>
2024-11-19 03:48:10 +0200

    feat: added price

--- script.js
+++ script.js
@@ -15,8 +15,7 @@
  * WebSocket URL for the Titan scanner service.
  * @constant {string}
  */
-//const titanScannerWsUrl = "wss://ws.stocktitan.net:9021/null";
-const titanScannerWsUrl = "wss://ws.stocktitan.net:9021/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjExNjY4NCwidXNlcm5hbWUiOiJzdmlkbGFrayIsInByZW1pdW0iOjAsInByZW1pdW1FeHBpcmF0aW9uIjpmYWxzZSwiZXhwIjoxNzMyMzAwMzU2LCJpYXQiOjE3MzE2OTU1NTZ9.4WZs5LWCt2cKTCJJBbIU1JIY7P7UMoo2Mnq8Na9axNM";
+const websocketUrl = "wss://websocket-momentum-euvvn.ondigitalocean.app/ws"
 
 /**
  * CSS class for light green background color.
@@ -68,24 +67,24 @@ const maxTableRows = 10;
  * @property {Array} sharesFloat - Thresholds for shares float.
  */
 const thresholds = {
-    volume: [
-        { max: 100_000, class: lightGreenClass },
-        { max: 250_000, class: mediumGreenClass },
-        { max: 500_000, class: darkGreenClass },
-        { max: Infinity, class: darkestGreenClass }
-    ],
-    marketCap: [
-        { max: 100_000_000, class: darkestGreenClass },
-        { max: 200_000_000, class: darkGreenClass },
-        { max: 1_000_000_000, class: mediumGreenClass },
-        { max: Infinity, class: lightGreenClass }
-    ],
-    sharesFloat: [
-        { max: 10_000_000, class: darkestGreenClass },
-        { max: 50_000_000, class: darkGreenClass },
-        { max: 100_000_000, class: mediumGreenClass },
-        { max: Infinity, class: lightGreenClass }
-    ]
+  volume: [
+    { max: 100_000, class: lightGreenClass },
+    { max: 250_000, class: mediumGreenClass },
+    { max: 500_000, class: darkGreenClass },
+    { max: Infinity, class: darkestGreenClass }
+  ],
+  marketCap: [
+    { max: 100_000_000, class: darkestGreenClass },
+    { max: 200_000_000, class: darkGreenClass },
+    { max: 1_000_000_000, class: mediumGreenClass },
+    { max: Infinity, class: lightGreenClass }
+  ],
+  sharesFloat: [
+    { max: 10_000_000, class: darkestGreenClass },
+    { max: 50_000_000, class: darkGreenClass },
+    { max: 100_000_000, class: mediumGreenClass },
+    { max: Infinity, class: lightGreenClass }
+  ]
 };
 
 /**
@@ -93,14 +92,14 @@ const thresholds = {
  * @param {StockData} data - The stock data.
  */
 function insertNewTableRow(data) {
-    const tableBody = document.querySelector('#dataTable tbody');
-    const row = createTableRow(data);
+  const tableBody = document.querySelector('#dataTable tbody');
+  const row = createTableRow(data);
 
-    if (tableBody.rows.length >= maxTableRows) {
-        tableBody.deleteRow(tableBody.rows.length - 1);
-    }
+  if (tableBody.rows.length >= maxTableRows) {
+    tableBody.deleteRow(tableBody.rows.length - 1);
+  }
 
-    tableBody.insertBefore(row, tableBody.firstChild);
+  tableBody.insertBefore(row, tableBody.firstChild);
 }
 
 /**
@@ -109,23 +108,23 @@ function insertNewTableRow(data) {
  * @returns {HTMLTableRowElement} The created table row element.
  */
 function createTableRow(data) {
-    const formattedData = {
-        volume: formatNumber(data.volume),
-        marketCap: formatNumber(data.market_cap),
-        sharesFloat: formatNumber(data.shares_float),
-        priceChangeRatio: formatPercentage(data.price_change_ratio),
-        hasNews: data.news.length > 0,
-        volumeBgClass: getBackgroundClass(data.volume, 'volume'),
-        marketCapBgClass: getBackgroundClass(data.market_cap, 'marketCap'),
-        sharesFloatBgClass: getBackgroundClass(data.shares_float, 'sharesFloat'),
-        timestamp: createNewDate(),
-        redirectUrl: generateNewsRedirecURL(data.symbol, data.internal_url),
-    };
+  const formattedData = {
+    volume: formatNumber(data.volume),
+    marketCap: formatNumber(data.market_cap),
+    sharesFloat: formatNumber(data.shares_float),
+    priceChangeRatio: formatPercentage(data.price_change_ratio),
+    hasNews: data.news.length > 0,
+    volumeBgClass: getBackgroundClass(data.volume, 'volume'),
+    marketCapBgClass: getBackgroundClass(data.market_cap, 'marketCap'),
+    sharesFloatBgClass: getBackgroundClass(data.shares_float, 'sharesFloat'),
+    timestamp: createNewDate(),
+    redirectUrl: generateNewsRedirecURL(data.symbol, data.internal_url),
+  };
 
-    const row = document.createElement('tr');
-    row.classList.add('hover:bg-gray-600', 'fade-in-row');
+  const row = document.createElement('tr');
+  row.classList.add('hover:bg-gray-600', 'fade-in-row');
 
-    row.innerHTML = `
+  row.innerHTML = `
         <td class="py-2 px-4">${data.symbol}</td>
         <td class="py-2 px-4">${data.price}</td>
         <td class="py-2 px-4 ${greenTextColor}">${formattedData.priceChangeRatio}</td>
@@ -136,7 +135,7 @@ function createTableRow(data) {
         <td class="py-2 px-4">${data.alert_count}</td>
         <td class="py-2 px-4">${formattedData.timestamp}</td>
     `;
-    return row;
+  return row;
 }
 
 /**
@@ -146,7 +145,7 @@ function createTableRow(data) {
  * @returns {string} The full URL for the stock news.
  */
 function generateNewsRedirecURL(stockName, internalUrl) {
-    return `https://www.stocktitan.net/news/${stockName}/${internalUrl}.html`;
+  return `https://www.stocktitan.net/news/${stockName}/${internalUrl}.html`;
 }
 
 /**
@@ -154,8 +153,8 @@ function generateNewsRedirecURL(stockName, internalUrl) {
  * @returns {string} The formatted current date and time.
  */
 function createNewDate() {
-    const date = new Date();
-    return date.toLocaleTimeString('en-GB', { hour12: false }) + ' ' + date.toLocaleDateString('en-GB');
+  const date = new Date();
+  return date.toLocaleTimeString('en-GB', { hour12: false }) + ' ' + date.toLocaleDateString('en-GB');
 }
 
 /**
@@ -164,7 +163,7 @@ function createNewDate() {
  * @returns {string} The formatted percentage string.
  */
 function formatPercentage(value) {
-    return (value * 100).toFixed(2) + '%';
+  return (value * 100).toFixed(2) + '%';
 }
 
 /**
@@ -173,7 +172,7 @@ function formatPercentage(value) {
  * @returns {string} The formatted number.
  */
 function formatNumber(value) {
-    return value.toLocaleString();
+  return value.toLocaleString();
 }
 
 /**
@@ -183,13 +182,13 @@ function formatNumber(value) {
  * @returns {string} The CSS class for the background color.
  */
 function getBackgroundClass(value, type) {
-    const ranges = thresholds[type];
+  const ranges = thresholds[type];
 
-    for (let i = 0; i < ranges.length; i++) {
-        if (value < ranges[i].max) {
-            return ranges[i].class;
-        }
+  for (let i = 0; i < ranges.length; i++) {
+    if (value < ranges[i].max) {
+      return ranges[i].class;
     }
+  }
 }
 
 /**
@@ -198,18 +197,18 @@ function getBackgroundClass(value, type) {
  * @param {string} changePercentage - The change percentage of the stock.
  */
 function insertHasntSeenBeforeStock(stockName, changePercentage) {
-    const indicator = document.getElementById('new-stock-indicator');
-    const stockInfo = document.getElementById('stock-info');
-    const stockNameElement = document.getElementById('stock-name');
-    const stockChangeElement = document.getElementById('stock-change');
-    const timestampElement = document.getElementById('stock-timestamp');
+  const indicator = document.getElementById('new-stock-indicator');
+  const stockInfo = document.getElementById('stock-info');
+  const stockNameElement = document.getElementById('stock-name');
+  const stockChangeElement = document.getElementById('stock-change');
+  const timestampElement = document.getElementById('stock-timestamp');
 
-    indicator.classList.remove('hidden');
-    stockInfo.classList.add('pulse-animation');
+  indicator.classList.remove('hidden');
+  stockInfo.classList.add('pulse-animation');
 
-    stockNameElement.innerText = stockName;
-    stockChangeElement.innerText = changePercentage;
-    timestampElement.innerText = createNewDate();
+  stockNameElement.innerText = stockName;
+  stockChangeElement.innerText = changePercentage;
+  timestampElement.innerText = createNewDate();
 }
 
 /**
@@ -218,20 +217,20 @@ function insertHasntSeenBeforeStock(stockName, changePercentage) {
  * @param {string} changePercentage - The change percentage of the stock.
  */
 function notifyAboutNewStock(stockName, changePercentage) {
-    if (typeof Notification !== "undefined" && Notification.permission === "granted") {
-        new Notification(`New Stock Added: ${stockName}`, {
-            body: `${stockName} has been added with a ${changePercentage} change.`
-        });
-    }
+  if (typeof Notification !== "undefined" && Notification.permission === "granted") {
+    new Notification(`New Stock Added: ${stockName}`, {
+      body: `${stockName} has been added with a ${changePercentage} change.`
+    });
+  }
 }
 
 /**
  * Requests permission for desktop notifications if not already granted.
  */
 function requestNotificationPermission() {
-    if (typeof Notification !== "undefined" && Notification.permission === "default") {
-        Notification.requestPermission();
-    }
+  if (typeof Notification !== "undefined" && Notification.permission === "default") {
+    Notification.requestPermission();
+  }
 }
 
 /**
@@ -243,12 +242,12 @@ function requestNotificationPermission() {
  * @returns {boolean} - Returns true if the stock value doesn't meet the threshold.
  */
 function doesNotMeetThreshold(stockValue, filterType, threshold) {
-    if (filterType === "above") {
-        return stockValue <= threshold;
-    } else if (filterType === "below") {
-        return stockValue >= threshold;
-    }
-    return false;
+  if (filterType === "above") {
+    return stockValue <= threshold;
+  } else if (filterType === "below") {
+    return stockValue >= threshold;
+  }
+  return false;
 }
 
 /**
@@ -258,26 +257,26 @@ function doesNotMeetThreshold(stockValue, filterType, threshold) {
  * @returns {boolean} - Returns true if the stock should be filtered out; otherwise false.
  */
 function isStockFilteredOutByUserSettings(data) {
-    const storage = localStorage.getItem("formData");
-    if (!storage || !storage.length) return false;
+  const storage = localStorage.getItem("formData");
+  if (!storage || !storage.length) return false;
 
-    const formData = JSON.parse(storage);
+  const formData = JSON.parse(storage);
 
-    if (
-        doesNotMeetThreshold(data.price, formData.price, formData.priceInput) ||
-        doesNotMeetThreshold(data.volume, formData.volumeSelect, formData.volume) ||
-        doesNotMeetThreshold(data.market_cap, formData.marketCapSelect, formData.marketCap) ||
-        doesNotMeetThreshold(data.shares_float, formData.sharesFloatSelect, formData.sharesFloat) ||
-        doesNotMeetThreshold(data.alert_count, formData.alertsNumberSelect, formData.alertsNumber)
-    ) {
-        return true;
-    }
+  if (
+    doesNotMeetThreshold(data.price, formData.price, formData.priceInput) ||
+    doesNotMeetThreshold(data.volume, formData.volumeSelect, formData.volume) ||
+    doesNotMeetThreshold(data.market_cap, formData.marketCapSelect, formData.marketCap) ||
+    doesNotMeetThreshold(data.shares_float, formData.sharesFloatSelect, formData.sharesFloat) ||
+    doesNotMeetThreshold(data.alert_count, formData.alertsNumberSelect, formData.alertsNumber)
+  ) {
+    return true;
+  }
 
-    if (formData.hasNewsTrue && data.news.length == 0) {
-        return true;
-    }
+  if (formData.hasNewsTrue && data.news.length == 0) {
+    return true;
+  }
 
-    return false;
+  return false;
 }
 
 /**
@@ -290,14 +289,14 @@ function isStockFilteredOutByUserSettings(data) {
  * @returns {void} - This function does not return anything. It performs an action of sending an email.
  */
 function sendErrorEmail(error, payload) {
-    const errorInfo = JSON.stringify({
-        userAgent: navigator.userAgent,
-        message: error.message,
-        stack: error.stack,
-        name: error.name,
-    }, null, 2);
+  const errorInfo = JSON.stringify({
+    userAgent: navigator.userAgent,
+    message: error.message,
+    stack: error.stack,
+    name: error.name,
+  }, null, 2);
 
-    const emailContent = `
+  const emailContent = `
         <h3>Error in Stock Processing</h3>
         <p><strong>Error Details:</strong></p>
         <pre>${errorInfo}</pre>
@@ -305,19 +304,19 @@ function sendErrorEmail(error, payload) {
         <pre>${JSON.stringify(payload, null, 2)}</pre>
     `;
 
-    fetch('https://api.brevo.com/v3/smtp/email', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'api-key': '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',
-        },
-        body: JSON.stringify({
-            sender: { email: 'svid320@gmail.com' },
-            to: [{ email: 'svid320@gmail.com' }],
-            subject: '[momentum-scanner] Error Occurred in Stock Processing',
-            htmlContent: emailContent,
-        }),
-    }).catch(() => { });
+  fetch('https://api.brevo.com/v3/smtp/email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'api-key': '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',
+    },
+    body: JSON.stringify({
+      sender: { email: 'svid320@gmail.com' },
+      to: [{ email: 'svid320@gmail.com' }],
+      subject: '[momentum-scanner] Error Occurred in Stock Processing',
+      htmlContent: emailContent,
+    }),
+  }).catch(() => { });
 };
 
 
@@ -325,40 +324,40 @@ function sendErrorEmail(error, payload) {
  * Initializes the WebSocket connection and handles incoming messages.
  */
 (function () {
-    requestNotificationPermission();
+  requestNotificationPermission();
 
-    const ws = new WebSocket(titanScannerWsUrl);
+  const ws = new WebSocket(webkitURL);
 
-    ws.onerror = (msg, ev) => {
-        console.log({ msg, ev });
-    }
+  ws.onerror = (msg, ev) => {
+    console.log({ msg, ev });
+  }
 
-    ws.onclose = (msg, ev) => {
-        console.log({ msg, ev });
-    }
+  ws.onclose = (msg, ev) => {
+    console.log({ msg, ev });
+  }
 
-    ws.onmessage = msg => {
-        const { data: dataStringified } = msg;
-        const { header: { type }, payload } = JSON.parse(dataStringified);
+  ws.onmessage = msg => {
+    const { data: dataStringified } = msg;
+    const { header: { type }, payload } = JSON.parse(dataStringified);
 
-        if (type !== "journal") return;
+    if (type !== "journal") return;
 
-        if (payload.price_change_ratio > 0) {
-            try {
-                if (isStockFilteredOutByUserSettings(payload)) return;
+    if (payload.price_change_ratio > 0) {
+      try {
+        if (isStockFilteredOutByUserSettings(payload)) return;
 
-                insertNewTableRow(payload);
+        insertNewTableRow(payload);
 
-                if (payload.alert_count === 1 && payload.news.length > 0) {
-                    insertHasntSeenBeforeStock(payload.symbol, formatPercentage(payload.price_change_ratio));
-                    notifyAboutNewStock(payload.symbol, formatPercentage(payload.price_change_ratio));
-                }
-            } catch (e) {
-                console.log({ e });
-                console.log({ payload });
-                sendErrorEmail(e, payload);
-            }
-
+        if (payload.alert_count === 1 && payload.news.length > 0) {
+          insertHasntSeenBeforeStock(payload.symbol, formatPercentage(payload.price_change_ratio));
+          notifyAboutNewStock(payload.symbol, formatPercentage(payload.price_change_ratio));
         }
+      } catch (e) {
+        console.log({ e });
+        console.log({ payload });
+        sendErrorEmail(e, payload);
+      }
+
     }
+  }
 })();
